e7989deb967d9f691999d26cd12e5e1e7a82556a
Shivam Dubey <dshivam227@gmail.com>
2023-01-06 01:07:53 +0530

    Airplane Seat Assignment Probability

---
+++ Microsoft/Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,9 @@
+// Link: https://leetcode.com/problems/airplane-seat-assignment-probability/description/
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if( n==1)
+        return 1.00000;
+        else return 0.5000;
+    }
+};
